9a11efab88e8af5286ed85b532f4c617c371667b
artdrozdov <artem.drozdoff@gmail.com>
2015-06-05 21:38:42 +0300

    Commit

---
+++ CrackR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrackR.Arrays;
+using CrackR.Lists;
+using CrackR.StacksAndQueues;
+
+namespace CrackR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new SetOfStacks<int>();
+            stack.Push(6);
+            stack.Push(5);
+            stack.Push(7);
+            for (int i = 0; i < 100; i++)
+            {
+                stack.Push(i);
+            }
+            //stack.Pop();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.PopAt(0));
+            stack.Push(666);
+            stack.Push(777);
+            Console.WriteLine("The beast:" + stack.PopAt(0));
+            stack.Push(111);
+            stack.Push(112);
+            stack.Push(113);
+            Console.WriteLine(stack.Pop());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CrackR/StacksAndQueues/MinStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrackR.Lists;
+
+namespace CrackR.StacksAndQueues
+{
+    public class MinStack<T> where T:IComparable<T>
+    {
+        private Node<T> _min { get; set; }
+        private Node<T> _heap { get; set; }
+
+        public void Push(T value)
+        {
+            if (_heap != null)
+            {
+                var newNode = new Node<T>(value);
+                newNode.Next = _heap;
+                _heap = newNode;
+                if (_heap.Value.CompareTo(_min.Value) <= 0)
+                {
+                    var node = new Node<T>(_heap.Value);
+                    node.Next = _min;
+                    _min = node;
+                }
+            }
+            else
+            {
+                _heap = new Node<T>(value);
+                
+                _min = new Node<T>(value);
+            }
+        }
+
+        public T Pop()
+        {
+            if (_heap == null)
+            {
+                throw new InvalidOperationException();
+            }
+            var result = _heap.Value;
+            _heap = _heap.Next;
+            if (result.CompareTo(_min.Value) == 0)
+            {
+                _min = _min.Next;
+            }
+            return result;
+        }
+
+        public T Min()
+        {
+            if(_min == null)
+                throw new InvalidOperationException();
+            return _min.Value;
+        }
+    }
+}
